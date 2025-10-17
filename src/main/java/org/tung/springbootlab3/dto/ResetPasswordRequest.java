8bd96f989d696589e26c785a2e632aee85a1f93c
Nguyen Anh Tung <atvn15@gmail.com>
2025-10-17 11:34:33 +0700

    add login with email, add api reset pasword

---
+++ src/main/java/org/tung/springbootlab3/dto/ResetPasswordRequest.java
@@ -0,0 +1,14 @@
+package org.tung.springbootlab3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+    private String email;
+    private String newPassword;
+    private String code;
+}
